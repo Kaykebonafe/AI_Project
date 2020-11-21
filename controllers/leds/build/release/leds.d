994e626f1e002fc23fe1ab947d371b50e28a64a8
Kayke Bonafé <kayke.bonafe98@gmail.com>
2020-11-21 19:09:02 -0300

    Project Done

---
+++ controllers/leds/build/release/leds.d
@@ -0,0 +1,9 @@
+build/release/leds.o: leds.c \
+ C:\Users\Kayke\AppData\Local\Programs\Webots/include/controller/c/webots/device.h \
+ C:\Users\Kayke\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\Kayke\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Kayke\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\Kayke\AppData\Local\Programs\Webots/include/controller/c/webots/led.h \
+ C:\Users\Kayke\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\Kayke\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\Kayke\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h
